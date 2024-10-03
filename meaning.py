fb8e179d0fe5858297e74498a869858fe0532a71
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-10-03 18:57:18 +0530

    added sample video
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- meaning.py
+++ meaning.py
@@ -1,40 +1,41 @@
 import pyttsx3
-from PyDictionary import PyDictionary
-
+import requests
 
 class Speaking:
+    def speak(self, audio):
+        engine = pyttsx3.init('sapi5')
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id)
+        engine.say(audio)
+        engine.runAndWait()
 
-	def speak(self, audio):
-	
-		# Having the initial constructor of pyttsx3 
-		# and having the sapi5 in it as a parameter
-		engine = pyttsx3.init('sapi5')
-		
-		# Calling the getter and setter of pyttsx3
-		voices = engine.getProperty('voices')
-		
-		# Method for the speaking of the assistant
-		engine.setProperty('voice', voices[1].id)
-		engine.say(audio)
-		engine.runAndWait()
+class Try:
+    def Dictionary(self):
+        speak = Speaking()
+        speak.speak("Which word do you want to find the meaning of, sir?")
+        
+        # Taking the string input
+        query = input()
+        meaning = self.get_meaning(query)
 
+        if meaning:
+            print("Meaning:", meaning)
+            speak.speak("The meaning is " + meaning)
+        else:
+            speak.speak("Sorry, I couldn't find the meaning for that word.")
 
-class Meaning:
-	def Dictionary(self):
-		speak = Speaking()
-		dic = PyDictionary()
-		speak.speak("Which word do u want to find the meaning sir")
-		
-		# Taking the string input
-		query = str(input())
-		word = dic.meaning(query)
-		print(len(word))
-		
-		for state in word:
-			print(word[state])
-			speak.speak("the meaning is" + str(word[state]))
-
+    def get_meaning(self, word):
+        # Use a different dictionary API here
+        # Replace with your API endpoint and key
+        url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            return data[0]['meanings'][0]['definitions'][0]['definition']
+        else:
+            return None
 
 if __name__ == '__main__':
-	Meaning()
-	Meaning.Dictionary(self=None)
+    gfg_instance = Try()
+    gfg_instance.Dictionary()
